042c4f91e4c2d632cd8c03747b4eb70e6fdfd986
sofigo1010 <110646163+sofigo1010@users.noreply.github.com>
2024-10-24 23:33:29 -0600

    lab

--- lab4/RendererOpenGl.py
+++ lab4/RendererOpenGl.py
@@ -1,10 +1,11 @@
 
 import pygame
 from pygame.locals import *
+from OpenGL.GL import *  
 from gl import Renderer
-from buffer import Buffer
+from buffer import *
 from shaders import *
-from model import Model
+from model import *
 import glm
 
 width = 540
@@ -34,7 +35,6 @@ vShader =vertex_shader
 fShader = fragment_shader
 rend.SetShaders(vShader, fShader)
 
-
 while isRunning: 
     deltaTime = clock.tick(60) / 1000.0
     keys = pygame.key.get_pressed() #teclas presionadas en este momento
@@ -48,30 +48,43 @@ while isRunning:
                 isRunning = False
                 
             if event.key == pygame.K_1:
-                rend.filledMode()
+                rend.FilledMode()
                 
             if event.key == pygame.K_2:
-                rend.wireframeMode()
+                rend.WireframeMode()
                 
             if event.key == pygame.K_3:
-                vShader =vertex_shader
+                explosion_intensity = 1.0  
+                explosion_center = glm.vec3(0.0, 0.0, 0.0)
+                vShader = explosion_shader
                 rend.SetShaders(vShader, fShader)
-           
+                glUseProgram(rend.active_shaders)
+                explosion_location = glGetUniformLocation(rend.active_shaders, "explosionIntensity")
+                explosion_center_location = glGetUniformLocation(rend.active_shaders, "explosionCenter")
+
+                if explosion_location != -1 and explosion_center_location != -1:
+                    glUniform1f(explosion_location, explosion_intensity)
+                    glUniform3fv(explosion_center_location, 1, glm.value_ptr(explosion_center))
+
             if event.key == pygame.K_4:
-                vShader =fat_shader
+                vShader =star_expand_shader
                 rend.SetShaders(vShader, fShader)
                 
             if event.key == pygame.K_5:
-                vShader =water_shader
+                vShader =spiral_shader
                 rend.SetShaders(vShader, fShader)
 
             if event.key == pygame.K_6:
-                fShader =fragment_shader
+                fShader = electric_shader 
                 rend.SetShaders(vShader, fShader)
 
             if event.key == pygame.K_7:
-                fShader =negative_shader
-                rend.SetShaders(vShader, fShader)                
+                fShader =aura_shader
+                rend.SetShaders(vShader, fShader)   
+
+            if event.key == pygame.K_8:
+                fShader =disintegration_shader
+                rend.SetShaders(vShader, fShader)                 
                 
 
   
@@ -92,7 +105,6 @@ while isRunning:
     
     if keys[K_PAGEDOWN]:
         rend.pointLight.y -= 1*deltaTime
-
 
     if keys[K_a]:
         rend.camera.position.x -= 1 * deltaTime
